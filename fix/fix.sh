91b8da9c41dc7fc714688941678ce29912697309
Martin Thong <martin@scantist.com>
2022-06-21 11:27:27 +0800

    updated fix.sh

--- fix/fix.sh
+++ fix/fix.sh
@@ -1,34 +1,35 @@
 #!/bin/bash
 
 # run poc to test vulnerable env
-cd ~/javascript/poc
+echo "*** Starting POC"
+cd /home/tester/javascript/poc
 node poc.js
 
 # check the presence of evilfile
-FILE=~/javascript/poc/evilfile
+FILE=/home/tester/javascript/poc/evilfile
 if [ -f "$FILE" ]; then
-    echo "$FILE exists."
-    echo "deleting evilfile and replacing git.js and running node poc.js"
-    rm ~/javascript/poc/evilfile
+    echo "*** $FILE exists."
+    echo "*** deleting evilfile and replacing git.js and running node poc.js"
+    rm /home/tester/javascript/poc/evilfile
 
     # replace lines in git.js
-    cd ~/javascript/node_modules/workspace-tools/lib
+    cd /home/tester/javascript/node_modules/workspace-tools/lib
     # sed -i 's/search_string/replace_string/' filename
     # fix both lines of code in git.js
     sed -i 's/git(\[\"fetch\",\ remote/git(\[\"fetch\",\ \"--\",\ remote/g' git.js
 
     # run poc again
-    cd ~/javascript/poc
+    cd /home/tester/javascript/poc
     node poc.js
     if [ -f "$FILE" ]; then
-        echo "POC fail."
+        echo "*** POC fail."
     else 
-        echo "POC success!!!."
+        echo "*** POC success!!!."
     fi
 else 
-    echo "$FILE does not exist."
+    echo "*** $FILE does not exist."
 fi
 
 # build npm package on local and upload to code artifact
-# cd ~/javascript/node_modules/workspace-tools/
+# cd /home/tester/javascript/node_modules/workspace-tools/
 # npm publish
